fa43306938d7155e4ec60912fd12dde8fff55d33
Vinícius César Bertolo Calligaris <vinicius.cesar7@hotmail.com>
2025-04-10 10:22:34 -0300

    Finalizado

--- manipular_vetores.v
+++ manipular_vetores.v
@@ -1,7 +1,8 @@
 module manipular_vetores( 
     input [31:0] entrada,
-    output [31:0] saida );
+    output [31:0] saida 
+);
 
-    // insira seu código aqui
+    assign saida = {entrada[7:0], entrada[15:8], entrada[23:16], entrada[31:24]};
 
 endmodule
